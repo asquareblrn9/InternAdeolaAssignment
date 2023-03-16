1e0b149414bbd913735f5fc2ac007116b9b4d16a
asquareblrn9 <asquareblrn9@gmail.com>
2023-03-16 10:49:12 +0100

    add assignment

---
+++ Branch.cs
@@ -0,0 +1,29 @@
+﻿using AdeolaConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdeolaConsoleApp
+{
+    internal class Branch
+    {
+        public List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+    }
+
+}
+var root = new Branch();
+var child1 = new Branch();
+var child2 = new Branch();
+
+root.branches.Add(child1);
+root.branches.Add(child2);
+
+var grandchild1 = new Branch();
+child1.branches.Add(grandchild1);
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the hour (0-12): ");
+        int hour = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the minute (0-59): ");
+        int minute = int.Parse(Console.ReadLine());
+
+        // hour hand moves 30 degrees per hour
+        double hourAngle = (hour % 12 + (double)minute / 60) * 30;
+
+        // minute hand moves 6 degrees per minute
+        double minuteAngle = minute * 6;
+
+        // get the absolute difference between the two angles
+        double angle = Math.Abs(hourAngle - minuteAngle); 
+
+        // to check and make sure the angle is not greater than 180 degrees
+        if (angle > 180)
+        {
+            angle = 360 - angle;
+        }
+
+        Console.WriteLine($"The lesser angle between the hour and minute hands is {angle} degrees.");
+    }
+}
